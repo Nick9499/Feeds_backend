473d426168a8e2684fad9006fd8e623bb58139fd
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-08-29 12:41:57 +0530

    static image & error handling

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(bodyparser.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,6 +19,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
